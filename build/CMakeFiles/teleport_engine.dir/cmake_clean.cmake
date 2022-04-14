32ff577da4aa4761055fc6ebeca6e43eed078a59
keyclicker <kolchikcv@ukr.net>
2022-04-14 19:58:43 +0300

    Some documentation added

---
+++ build/CMakeFiles/teleport_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teleport_engine.dir/Engine/Game.cpp.o"
+  "CMakeFiles/teleport_engine.dir/Engine/Game.cpp.o.d"
+  "CMakeFiles/teleport_engine.dir/Game/main.cpp.o"
+  "CMakeFiles/teleport_engine.dir/Game/main.cpp.o.d"
+  "teleport_engine"
+  "teleport_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teleport_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
